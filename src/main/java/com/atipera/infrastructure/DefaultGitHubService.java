ba3e03f51d68589315f23c9069b7de9ec339cfa1
Michał Napora <napora.michael@gmail.com>
2025-03-10 06:52:26 +0100

    Modified to return empty list if user has no repos

--- src/main/java/com/atipera/infrastructure/DefaultGitHubService.java
+++ src/main/java/com/atipera/infrastructure/DefaultGitHubService.java
@@ -11,21 +11,19 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @ApplicationScoped
 public class DefaultGitHubService implements GitHubService {
-  private static final String GITHUB_USER_OR_REPOSITORIES_NOT_FOUND =
-      "GitHub user not found or has no repositories";
+  private static final String GITHUB_USER_NOT_FOUND = "GitHub user not found";
   @Inject @RestClient GitHubClient client;
 
   public Uni<List<RepositoryResponseDto>> getUserRepositoriesDto(final String username) {
     return client
         .getUserRepositories(username)
         .onFailure(WebApplicationException.class)
-        .transform(throwable -> new NotFoundException(GITHUB_USER_OR_REPOSITORIES_NOT_FOUND))
+        .transform(throwable -> new NotFoundException(GITHUB_USER_NOT_FOUND))
         .onItem()
         .transformToUni(
             repos -> {
               if (repos == null || repos.isEmpty()) {
-                return Uni.createFrom()
-                    .failure(new NotFoundException(GITHUB_USER_OR_REPOSITORIES_NOT_FOUND));
+                return Uni.createFrom().item(List.of());
               }
               final var repoUnis =
                   repos.stream()
